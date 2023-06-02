3a95519945b8154b5d85dd807508a281c1d98b1f
Guilherme Henrique de Oliveira <guihenrique_oliveira@hotmail.com>
2023-06-02 17:22:22 -0300

    Updated login router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DescriptionComponent } from './components/description/description.component';
+import { LoginComponent } from './components/login/login.component';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: DescriptionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
